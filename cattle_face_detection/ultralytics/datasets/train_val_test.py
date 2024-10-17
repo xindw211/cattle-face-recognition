aaa53dd70c5691833c5b67d50f341bea64eac635
小白 <96227034+xindw211@users.noreply.github.com>
2024-10-17 19:49:44 +0800

    Add files via upload

---
+++ cattle_face_detection/ultralytics/datasets/train_val_test.py
@@ -0,0 +1,72 @@
+import os, shutil, random
+from tqdm import tqdm
+
+"""
+标注文件是yolo格式（txt文件）
+训练集：验证集：测试集 （7：2：1） 
+"""
+
+
+def split_img(img_path, label_path, split_list):
+    try:
+        Data = 'cow'
+
+        train_img_dir = Data + '/images/train'
+        val_img_dir = Data + '/images/val'
+        test_img_dir = Data + '/images/test'
+
+        train_label_dir = Data + '/labels/train'
+        val_label_dir = Data + '/labels/val'
+        test_label_dir = Data + '/labels/test'
+
+        os.makedirs(train_img_dir)
+        os.makedirs(train_label_dir)
+        os.makedirs(val_img_dir)
+        os.makedirs(val_label_dir)
+        os.makedirs(test_img_dir)
+        os.makedirs(test_label_dir)
+
+    except:
+        print('文件目录已存在')
+
+    train, val, test = split_list
+    all_img = os.listdir(img_path)
+    all_img_path = [os.path.join(img_path, img) for img in all_img]
+
+    train_img = random.sample(all_img_path, int(train * len(all_img_path)))
+
+    train_label = [toLabelPath(img, label_path) for img in train_img]
+
+    for i in tqdm(range(len(train_img)), desc='train ', ncols=80, unit='img'):
+        _copy(train_img[i], train_img_dir)
+        _copy(train_label[i], train_label_dir)
+        all_img_path.remove(train_img[i])
+    val_img = random.sample(all_img_path, int(val / (val + test) * len(all_img_path)))
+    val_label = [toLabelPath(img, label_path) for img in val_img]
+    for i in tqdm(range(len(val_img)), desc='val ', ncols=80, unit='img'):
+        _copy(val_img[i], val_img_dir)
+        _copy(val_label[i], val_label_dir)
+        all_img_path.remove(val_img[i])
+    test_img = all_img_path
+    test_label = [toLabelPath(img, label_path) for img in test_img]
+    for i in tqdm(range(len(test_img)), desc='test ', ncols=80, unit='img'):
+        _copy(test_img[i], test_img_dir)
+        _copy(test_label[i], test_label_dir)
+
+
+def _copy(from_path, to_path):
+    shutil.copy(from_path, to_path)
+
+
+def toLabelPath(img_path, label_path):
+    img = img_path.split('/')[-1]
+    label = img.split('.jpg')[0] + '.txt'
+    return os.path.join(label_path, label)
+
+
+if __name__ == '__main__':
+    img_path = './data/images'
+    label_path = './data/labels'
+    split_list = [0.7, 0.2, 0.1]
+    split_img(img_path, label_path, split_list)
+
